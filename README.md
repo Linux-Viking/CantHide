a82c5a63d075d54b93b38aa8cdf711b0ed2b9fb3
Linux-Viking <22947358+Linux-Viking@users.noreply.github.com>
2024-03-10 18:04:56 -0500

    Updated README with more dependancies and included more archive formats.

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
-canthide.sh is a versatile file analysis and extraction tool designed for uncovering hidden data within different file formats. It supports Zip and 7-zip archives and extracts plain text from binary files. Ideal for data forensics and cybersecurity, the script automates data extraction, simplifying the process of revealing concealed information.
+canthide.sh is a versatile file analysis and extraction tool designed for uncovering hidden data within different file formats. It supports multiple archive formats and extracts plain text from binary files. Ideal for data forensics and cybersecurity, the script automates data extraction, simplifying the process of revealing concealed information.
 
 Installation
 Dependencies
 
-Before running CantHide.sh, you need to install the following dependencies:
+Before running canthide.sh, you need to install the following dependencies:
 unzip
 p7zip
 steghide
 exiftool
+ar
+cpio
+bzip2
+tar
+lzip
+lz4
+lzma
+lzop
+xz-utils
+gzip
 
 For Debian-based systems (Ubuntu, etc.):
-`sudo apt-get update && sudo apt-get install -y unzip p7zip-full steghide libimage-exiftool-perl`
+`sudo apt-get update -y && sudo apt-get install unzip 7zip cpio bzip2 tar lzip lz4 lzma lzop xz-utils gzip steghide libimage-exiftool-perl -y`
 
 For Red Hat-based systems (Fedora, CentOS, etc.):
 
-`sudo yum install -y unzip p7zip p7zip-plugins steghide perl-Image-ExifTool`
+`sudo yum update -y && sudo yum install unzip p7zip cpio bzip2 tar lzip lz4 xz gzip steghide perl-Image-ExifTool -y`
 
 Usage
 
@@ -25,7 +35,7 @@ The script will automatically detect the file type and perform the necessary ext
 
 Features
 
-Supports extraction of Zip and 7-zip archives.
+Supports extraction of mutliple archive formats.
 Extracts plain text from binary files.
 Attempts to use steghide to extract hidden data from files, with or without a passphrase.
 Exports extracted strings and metadata to separate files for further analysis.
--- canthide.sh
+++ canthide.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-command -v unzip &>/dev/null || { echo "Error: 'unzip' is not installed." >&2; exit 1; } && command -v 7z &>/dev/null || { echo "Error: '7z' is not installed." >&2; exit 1; } && command -v steghide &>/dev/null || { echo "Error: 'steghide' is not installed." >&2; exit 1; } && command -v exiftool &>/dev/null || { echo "Error: 'exiftool' is not installed." >&2; exit 1; }
+#Dependency check 
+command -v unzip &>/dev/null || { echo "Error: 'unzip' is not installed." >&2; exit 1; } 
+command -v 7z &>/dev/null || { echo "Error: '7z' is not installed." >&2; exit 1; } 
+command -v ar &>/dev/null || { echo "Error: 'ar' is not installed." >&2; exit 1; }
+command -v cpio &>/dev/null || { echo "Error: 'cpio' is not installed." >&2; exit 1; }
+command -v bzip2 &>/dev/null || { echo "Error: 'bzip2' is not installed." >&2; exit 1; }
+command -v tar &>/dev/null || { echo "Error: 'tar' is not installed." >&2; exit 1; }
+command -v lzip &>/dev/null || { echo "Error: 'lzip' is not installed." >&2; exit 1; }
+command -v lz4 &>/dev/null || { echo "Error: 'lz4' is not installed." >&2; exit 1; }
+command -v lzma &>/dev/null || { echo "Error: 'lzma' is not installed." >&2; exit 1; }
+command -v lzop &>/dev/null || { echo "Error: 'lzop' is not installed." >&2; exit 1; }
+command -v xz &>/dev/null || { echo "Error: 'xz' is not installed." >&2; exit 1; }
+command -v gzip &>/dev/null || { echo "Error: 'gzip' is not installed." >&2; exit 1; }
+command -v steghide &>/dev/null || { echo "Error: 'steghide' is not installed." >&2; exit 1; } 
+command -v exiftool &>/dev/null || { echo "Error: 'exiftool' is not installed." >&2; exit 1; }
 
 target=$1
 
@@ -11,21 +25,80 @@ echo "File type is $info"
 # Unzips if archvie 
 
 if [[ $info == *"Zip"* ]]; then
-    unzip "$target" >/dev/null 2>&1
+    unzip -o "$target" >/dev/null 2>&1
     echo "Unzipped $target"
 
 elif [[ $info == *"7-zip"* ]]; then
     7z x "$target" -aoa >/dev/null 2>&1
     echo "Unzipped $target"
+
+elif [[ $info == *"ar archive"* ]]; then 
+    ar -x "$target" >/dev/null 2>&1
+    echo "Unzipped $target"
+
+elif [[ $info == *"cpio archive"* ]]; then
+    cpio -idu < "$target" >/dev/null 2>&1
+    echo "Unzipped $target"
+
+elif [[ $info == *"bzip2"* ]]; then
+    bzip2 -df "$target" >/dev/null 2>&1
+    echo "Unzipped $target"
+
+elif [[ $info == *"tar"* ]]; then
+    tar -xf "$target" --overwrite >/dev/null 2>&1
+    echo "Unzipped $target"
+
+elif [[ $info == *"lzip"* ]]; then 
+    lzip -df "$target" >/dev/null 2>&1
+    echo "Unzipped $target"
+
+elif [[ $info == *"LZ4"* ]]; then 
+    lz4 -df "$target" "$target.out" >/dev/null 2>&1
+    echo "Unzipped $target"
+
+elif [[ $info == *"LZMA"* ]]; then 
+    if [[ "${target##*.}" != "lzma" ]]; then
+        mv "$target" "${target}.lzma"
+        target="${target}.lzma"
+    fi
+    lzma -df "$target" >/dev/null 2>&1
+    echo "Unzipped $target"
+
+elif [[ $info == *"lzo"* ]]; then 
+    if [[ "${target##*.}" != "lzo" ]]; then
+        mv "$target" "${target}.lzo"
+        target="${target}.lzo"
+    fi
+    lzop -df "$target" >/dev/null 2>&1
+    echo "Unzipped $target"
+
+elif [[ $info == *"XZ"* ]]; then 
+    if [[ "${target##*.}" != "xz" ]]; then
+        mv "$target" "${target}.xz"
+        target="${target}.xz"
+    fi
+    xz -df "$target" >/dev/null 2>&1
+    echo "Unzipped $target"
+
+elif [[ $info == *"gzip"* ]]; then 
+    if [[ "${target##*.}" != "gz" ]]; then
+        mv $target "${target}.gz"
+        target="${target}.gz"
+    fi
+    gzip -df "$target" >/dev/null 2>&1
+    echo "Unzipped $target"
+
 else 
-# Attempts to use steghide with or without passphrase and output strings to file
+    # Attempts to use steghide with or without passphrase and output strings to file
     read -p "Enter a passphrase if supplied (leave blank if not provided): " passphrase
     echo "Trying steghide..."
     steghide --extract --passphrase "$passphrase" -sf "$target" -f 
+
+    # Extract the strings and metadata to respective files
+    echo "Exporting strings..."
+    strings "$target" > "$filename"_strings.txt
+    echo "Exporting metadata..." 
+    exiftool "$target" > "$filename"_metadata.txt
 fi
 
-# Extract the strings and metadata to respective files
-echo "Exporting strings..."
-strings "$target" > "$filename"_strings.txt
-echo "Exporting metadata..." 
-exiftool "$target" > "$filename"_metadata.txt
+
