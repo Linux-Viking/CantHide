1c6b4a0e8c49cc49c808ab8a20d6ab09b35b2bfa
Linux-Viking <22947358+Linux-Viking@users.noreply.github.com>
2024-03-16 18:50:56 -0500

    Added ASCII art banner, implemented target directory override with second argument, updated archive extraction functions to use the target directory, and added additional archive support.

--- README.md
+++ README.md
@@ -4,42 +4,49 @@ Installation
 Dependencies
 
 Before running canthide.sh, you need to install the following dependencies:
-unzip
-p7zip
-steghide
-exiftool
-cpio
-bzip2
-tar
-lzip
-lz4
-lzma
-lzop
-xz-utils
-gzip
-binwalk
+* unzip
+* p7zip
+* steghide
+* exiftool
+* cpio
+* bzip2
+* tar
+* lzip
+* lz4
+* lzma
+* lzop
+* xz-utils
+* gzip
+* binwalk
+* sharutils
 
 For Debian-based systems (Ubuntu, etc.):
-`sudo apt-get update -y && sudo apt-get install unzip 7zip cpio bzip2 tar lzip lz4 lzma lzop xz-utils gzip steghide libimage-exiftool-perl binwalk -y`
+`sudo apt-get update -y && sudo apt-get install unzip 7zip cpio bzip2 tar lzip lz4 lzma lzop xz-utils gzip sharutils steghide libimage-exiftool-perl binwalk -y`
 
 For Red Hat-based systems (Fedora, CentOS, etc.):
 
-`sudo yum update -y && sudo yum install unzip p7zip cpio bzip2 tar lzip lz4 xz gzip steghide perl-Image-ExifTool binwalk -y`
+`sudo yum update -y && sudo yum install unzip p7zip cpio bzip2 tar lzip lz4 xz gzip shartutils steghide perl-Image-ExifTool binwalk -y`
 
 Usage
 
 To use canthide.sh, simply provide the target file as an argument:
 `./canthide.sh target_file`
 
-The script will automatically detect the file type and perform the necessary extraction, revealing any hidden data within the file.
+An optional secondary argument can be used to set the target directory for the output: `./canthide.sh target_file [target_directory]`
+
+If `target_directory` is not provided, the directory of `target_file` will be used.
+
+If target_directory is not provided, the directory of `target_file` will be used.
+
+The script will detect the file type and prompt for extraction, revealing any hidden data within the file.
 
 Features
 
-Supports extraction of mutliple archive formats.
-Extracts plain text from binary files.
-Attempts to use steghide to extract hidden data from files, with or without a passphrase.
-Attempts to use binwalk to extract embedded files.
-Exports extracted strings and metadata to separate files for further analysis.
+* Supports extraction of mutliple archive formats.
+* Extracts plain text from binary files.
+* Attempts to use steghide to extract hidden data from files, with or without a  passphrase.
+* Attempts to use binwalk to extract embedded files.
+* Exports extracted strings and metadata to separate files for further analysis.
 
 License
 
--- canthide.sh
+++ canthide.sh
@@ -1,119 +1,247 @@
 #!/bin/bash
 # Authored by Linux Viking (https://github.com/Linux-Viking)
 
-# Dependency check 
-
-command -v unzip &>/dev/null || { echo "Error: 'unzip' is not installed." >&2; exit 1; } 
-command -v 7z &>/dev/null || { echo "Error: '7z' is not installed." >&2; exit 1; } 
-command -v ar &>/dev/null || { echo "Error: 'ar' is not installed." >&2; exit 1; }
-command -v cpio &>/dev/null || { echo "Error: 'cpio' is not installed." >&2; exit 1; }
-command -v bzip2 &>/dev/null || { echo "Error: 'bzip2' is not installed." >&2; exit 1; }
-command -v tar &>/dev/null || { echo "Error: 'tar' is not installed." >&2; exit 1; }
-command -v lzip &>/dev/null || { echo "Error: 'lzip' is not installed." >&2; exit 1; }
-command -v lz4 &>/dev/null || { echo "Error: 'lz4' is not installed." >&2; exit 1; }
-command -v lzma &>/dev/null || { echo "Error: 'lzma' is not installed." >&2; exit 1; }
-command -v lzop &>/dev/null || { echo "Error: 'lzop' is not installed." >&2; exit 1; }
-command -v xz &>/dev/null || { echo "Error: 'xz' is not installed." >&2; exit 1; }
-command -v gzip &>/dev/null || { echo "Error: 'gzip' is not installed." >&2; exit 1; }
-command -v steghide &>/dev/null || { echo "Error: 'steghide' is not installed." >&2; exit 1; } 
-command -v exiftool &>/dev/null || { echo "Error: 'exiftool' is not installed." >&2; exit 1; }
-command -v binwalk &>/dev/null || { echo "Error: 'binwalk' is not installed." >&2; exit 1; }
+# Target file and directory
 
 target=$1
+target_dir=${2:-$(dirname "$target")}
 
-# Help message 
+# Dependency array 
+
+dependencies=(unzip 7z shar ar cpio bzip2 tar lzip lz4 lzma lzop xz gzip steghide exiftool binwalk)
+
+# Help message check 
 
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo "To use canthide.sh, simply provide the target file as an argument: ./canthide.sh target_file"
+    help_message
+fi
+
+# Functions
+
+# Banner
+
+display_banner () {
+    cat << "EOF"
+ ██████╗ █████╗ ███╗   ██╗████████╗    
+██╔════╝██╔══██╗████╗  ██║╚══██╔══╝    
+██║     ███████║██╔██╗ ██║   ██║       
+██║     ██╔══██║██║╚██╗██║   ██║       
+╚██████╗██║  ██║██║ ╚████║   ██║       
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝       
+                                       
+██╗  ██╗██╗██████╗ ███████╗            
+██║  ██║██║██╔══██╗██╔════╝            
+███████║██║██║  ██║█████╗              
+██╔══██║██║██║  ██║██╔══╝              
+██║  ██║██║██████╔╝███████╗            
+╚═╝  ╚═╝╚═╝╚═════╝ ╚══════╝            
+                                       
+                by Linux Viking
+                
+EOF
+}
+
+
+
+
+# Help message output
+
+help_message () {
+    echo "Usage: ./canthide.sh target_file [target_directory]"
+    echo "If target_directory is not provided, the directory of target_file will be used."
     exit 0
-fi
+}
 
-info=$(file "$target" | cut -d ":" -f 2 | cut -c 2- | cut -d "," -f 1)
-filename=$(basename "$target")
-echo "File type is $info"
+# Makes sure target is not empty and exists 
 
-# Unzips if archvie 
-
-if [[ $info == *"Zip"* ]]; then
-    unzip -o "$target" >/dev/null 2>&1
-    echo "Unzipped $target"
-
-elif [[ $info == *"7-zip"* ]]; then
-    7z x "$target" -aoa >/dev/null 2>&1
-    echo "Unzipped $target"
-
-elif [[ $info == *"ar archive"* ]]; then 
-    ar -x "$target" >/dev/null 2>&1
-    echo "Unzipped $target"
-
-elif [[ $info == *"cpio archive"* ]]; then
-    cpio -idu < "$target" >/dev/null 2>&1
-    echo "Unzipped $target"
-
-elif [[ $info == *"bzip2"* ]]; then
-    bzip2 -df "$target" >/dev/null 2>&1
-    echo "Unzipped $target"
-
-elif [[ $info == *"tar"* ]]; then
-    tar -xf "$target" --overwrite >/dev/null 2>&1
-    echo "Unzipped $target"
-
-elif [[ $info == *"lzip"* ]]; then 
-    lzip -df "$target" >/dev/null 2>&1
-    echo "Unzipped $target"
-
-elif [[ $info == *"LZ4"* ]]; then 
-    lz4 -df "$target" "$target.out" >/dev/null 2>&1
-    echo "Unzipped $target"
-
-# Last four archives require respective extensions 
-elif [[ $info == *"LZMA"* ]]; then 
-    if [[ "${target##*.}" != "lzma" ]]; then
-        mv "$target" "${target}.lzma"
-        target="${target}.lzma"
+target_check () {
+    if [[ -z "$target" || ! -f "$target" ]]; then
+        e  cho "Error: Target file is not provided or does not exist."
+        exit 1
     fi
-    lzma -df "$target" >/dev/null 2>&1
-    echo "Unzipped $target"
+}
 
-elif [[ $info == *"lzo"* ]]; then 
-    if [[ "${target##*.}" != "lzo" ]]; then
-        mv "$target" "${target}.lzo"
-        target="${target}.lzo"
-    fi
-    lzop -df "$target" >/dev/null 2>&1
-    echo "Unzipped $target"
+# Checks for dependencies 
 
-elif [[ $info == *"XZ"* ]]; then 
-    if [[ "${target##*.}" != "xz" ]]; then
-        mv "$target" "${target}.xz"
-        target="${target}.xz"
-    fi
-    xz -df "$target" >/dev/null 2>&1
-    echo "Unzipped $target"
+dependency_check () {
+    for dependency in ${dependencies[@]}; do 
+        command -v "$dependency" &>/dev/null || { echo "Error: '$dependency' is not installed." >&2; exit 1; }
+    done 
+}
 
-elif [[ $info == *"gzip"* ]]; then 
-    if [[ "${target##*.}" != "gz" ]]; then
-        mv $target "${target}.gz"
-        target="${target}.gz"
-    fi
-    gzip -df "$target" >/dev/null 2>&1
-    echo "Unzipped $target"
+# Gather file info and retrieve basename
 
-else 
-    # Attempts to use steghide with or without passphrase and output strings to file
-    read -p "Enter a passphrase if supplied (leave blank if not provided): " passphrase
-    echo "Trying steghide..."
-    steghide --extract --passphrase "$passphrase" -sf "$target" -f 
+file_check () {
+    info=$(file "$target" | cut -d ":" -f 2 | cut -c 2- | cut -d "," -f 1)
+    filename=$(basename "$target")
+    echo "File type is $info"
+}
 
-    # Attempts to extract embedded files with binwalk
-    echo "Trying binwalk..."
-    binwalk -e "$target"
+# Unzips if supported archive
 
-    # Extracts strings and metadata to respective files
-    echo "Exporting strings..."
-    strings "$target" > "$filename"_strings.txt
-    echo "Exporting metadata..." 
-    exiftool "$target" > "$filename"_metadata.txt
-fi
+archive_check () {
+    while true; do
+        read -p "Unzip if supported archive? Type 'yes' or 'no': " option
+        if [[ $option == "yes" ]]; then
+            if [[ $info == *"Zip"* ]]; then
+                unzip -o "$target" -d "$target_dir" >/dev/null 2>&1
+                echo "Unzipped $filename to $target_dir"
+        
+            elif [[ $info == *"7-zip"* ]]; then
+                7z x "$target" -aoa -o "$target_dir" >/dev/null 2>&1
+                echo "Unzipped $filename to $target_dir"
+
+            elif [[ $info == *"ar archive"* ]]; then 
+                pushd "$target_dir" >/dev/null 2>&1
+                ar -x "$filename" >/dev/null 2>&1
+                echo "Unzipped $filename to $target_dir"
+                popd >/dev/null 2>&1
+
+            elif [[ $info == *"cpio archive"* ]]; then
+                pushd "$target_dir" >/dev/null 2>&1
+                cpio -idu < "$filename" >/dev/null 2>&1
+                echo "Unzipped $filename to $target_dir"
+                popd >/dev/null 2>&1
+
+            elif [[ $info == *"bzip2"* ]]; then
+                pushd "$target_dir" >/dev/null 2>&1
+                bzip2 -df "$filename" >/dev/null 2>&1
+                echo "Unzipped $filename to $target_dir"
+                popd >/dev/null 2>&1
+
+            elif [[ $info == *"tar"* ]]; then
+                tar -xf "$target" -C "$target_dir" --overwrite >/dev/null 2>&1
+                echo "Unzipped $filename to $target_dir"
+
+            elif [[ $info == *"lzip"* ]]; then 
+                lzip -df "$target" > "$target_dir/${filename%.lz}" 2>dev/null
+                echo "Unzipped $filename to $target_dir"
+
+            elif [[ $info == *"LZ4"* ]]; then 
+                lz4 -df "$target" "$target_dir/${filename%.lz4}" >/dev/null 2>&1
+                echo "Unzipped $filename to $target_dir"
+
+            elif [[ $info == "shell archive text" ]]; then 
+                shar -x "$target" > "$target.sh" 2>&1
+                echo "Unzipped $filename to $target_dir"
+
+            # Last four archives require respective extensions 
+
+            elif [[ $info == *"LZMA"* ]]; then 
+                if [[ "${target##*.}" != "lzma" ]]; then
+                    mv "$target" "${target}.lzma"
+                    target="${filename}.lzma"
+                fi
+                lzma -df "$target" > "$target_dir/${filename%.lzma}" 2>/dev/null
+                echo "Unzipped $filename to $target_dir"
+            
+            elif [[ $info == *"lzo"* ]]; then 
+                if [[ "${target##*.}" != "lzo" ]]; then
+                    mv "$target" "${target}.lzo"
+                    target="${filename}.lzo"
+                fi
+                lzop -df "$target" > "$target_dir/${filename%.lzo}" 2>/dev/null
+                echo "Unzipped $filename to $target_dir"
+            
+            elif [[ $info == *"XZ"* ]]; then 
+                if [[ "${target##*.}" != "xz" ]]; then
+                    mv "$target" "${target}.xz"
+                    target="${filename}.xz"
+                fi
+                xz -df "$target" > "$target_dir/${filename%.xz}" 2>/dev/null
+                echo "Unzipped $filename to $target_dir"
+
+            elif [[ $info == *"gzip"* ]]; then 
+                if [[ "${target##*.}" != "gz" ]]; then
+                    mv $target "${target}.gz"
+                    target="${filename}.gz"
+                fi
+                gzip -df "$target" > "$target_dir/${filename%.gz}" 2>/dev/null
+                echo "Unzipped $filename to $target_dir"
+
+            else 
+                echo "$filename is not a supported archive"
+                
+            fi
+            break
+
+        elif [[ $option == "no" ]]; then
+            break
+        else 
+            echo "Invalid input. Please type 'yes' or 'no': "
+        fi
+    done
+}
+
+# Attempts steghide with or without a passphrase
+
+try_steghide () {
+    while true; do
+        read -p "Try steghide? Type 'yes' or 'no': " option
+        if [[ $option == "yes" ]]; then
+            read -p "Enter a passphrase if supplied (leave blank if not provided): " passphrase
+            echo "Trying steghide..."
+            pushd "$target_dir" >/dev/null 2>&1
+            steghide --extract --passphrase "$passphrase" -sf "$target" -f &>/dev/null || { echo "Steghide: $filename is not a supported format."; }
+            popd >/dev/null 2>&1
+            break
+        elif [[ $option == "no" ]]; then
+            break
+        else 
+            echo "Invalid input. Please type 'yes' or 'no': "
+        fi
+    done
+}
+
+# Attemps to use binwalk
+
+try_binwalk () {
+    while true; do
+        read -p "Try binwalk? Type 'yes' or 'no': " option
+        if [[ $option == "yes" ]]; then
+            echo "Trying binwalk..."
+            pushd "$target_dir" >/dev/null 2>&1
+            binwalk -e "$target"
+            popd >/dev/null 2>&1
+            break
+        elif [[ $option == "no" ]]; then
+            break
+        else 
+            echo "Invalid input. Please type 'yes' or 'no': "
+        fi
+    done
+}
+
+# Exports strings and exiftool data to respective files
+
+export_data () {
+    while true; do   
+        read -p "Export data? Type 'yes' or 'no': " option
+        if [[ $option == "yes" ]]; then
+            echo "Exporting strings..."
+            strings "$target" > "$target_dir/$filename"_strings.txt
+            echo "Exporting metadata..." 
+            exiftool "$target" > "$target_dir/$filename"_metadata.txt
+            break
+        elif [[ $option == "no" ]]; then
+            break
+        else 
+            echo "Invalid input. Please type 'yes' or 'no': "
+        fi
+    done
+}
+
+# Function calls and exit
+
+display_banner
+target_check
+dependency_check
+file_check
+archive_check
+try_steghide
+try_binwalk
+export_data
+exit 0   
+
 
 
